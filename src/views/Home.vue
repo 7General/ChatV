1d58ea4246316763916640654048cc7d495bd8c9
左车骑校威卫 <>
2023-03-28 11:23:27 +0800

    [commits]初始化工程文件0328 [version]1.0.0 [jir_id]

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,36 @@
 <template>
-  welcome home
-  <button @click="dddd">ggo</button>
+  <div class="chatitem" @click="chatClick">聊天模式</div>
+  <div class="chatitem" @click="chatimageClick">图片模式</div>
 </template>
 
 <script>
-import { getCurrentInstance,reactive} from "vue";
 import { useRouter } from "vue-router";
 export default {
   setup() {
-    const router = useRouter()
-    const dddd = () => {
-      console.log("dddddddddddddddd");
+    const router = useRouter();
+    const chatClick = () => {
+      router.push({
+        path: "/about",
+      });
+    };
+    const chatimageClick = () => {
       router.push({
         path: "/about",
       });
     };
     return {
-      dddd,
+      chatClick,
+      chatimageClick
     };
   },
 };
 </script>
+
+<style scoped>
+.chatitem {
+  font-size: 2em;
+  border: 1px solid grey;
+  padding: 1em;
+  margin-bottom: 20px;
+}
+</style>
