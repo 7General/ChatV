197154cd147d82bad8c6ac21799247348d6b2b0a
左车骑校威卫 <>
2023-03-30 14:22:28 +0800

    [commits]生成图片 [version]1.0.0 [jir_id]

--- src/views/ChatImg.vue
+++ src/views/ChatImg.vue
@@ -8,22 +8,15 @@
         :src="item.url"
       />
     </div>
-    <!-- <div class="imgItem">
-      <img
-        src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-IDkBTIigZSgVus211GzoqSI0/user-DAkv6di4FmGJfYQZpjOtmuB8/img-l3bCmqMFmMdJCNZ5FqJ3MD2q.png?st=2023-03-30T02%3A20%3A17Z&se=2023-03-30T04%3A20%3A17Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-29T16%3A28%3A11Z&ske=2023-03-30T16%3A28%3A11Z&sks=b&skv=2021-08-06&sig=QJH/Wmvcmu13zXtrkJt0mra%2BGBGwCGSsPPCoS9taVBE%3D"
-        alt=""
-      />
-    </div> -->
   </div>
 </template>
 <style scoped>
 .imgItem {
-  margin-right: 10px;
+  margin-right: 5px;
   float: left;
 }
 .imgItem img {
-  width: 200px;
-  height: 200px;
+  width: 300px;
 }
 </style>
 
@@ -41,9 +34,11 @@ export default {
       listImge: [],
     });
     const sendChat = async () => {
-      console.log("image");
+      console.log("生成图片中....");
+      ChatImgData.listImge = [];
       const request = <ChatImageItem>{
         prompt: ChatImgData.inputValue,
+        n:4
       };
       const BASE_URL = "https://api.openai.com/v1/images/generations";
       const response = await axios.post(BASE_URL, request, {
